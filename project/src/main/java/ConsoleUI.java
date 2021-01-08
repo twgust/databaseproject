108e593213e7c4c1ed7318ea85c0ebff2aa5a8d4
Repsakras <62218681+Repsakras@users.noreply.github.com>
2021-01-08 19:38:53 +0100

    The UI is pretty much done, it just needs interaction with the db.

--- project/src/main/java/ConsoleUI.java
+++ project/src/main/java/ConsoleUI.java
@@ -1,6 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.Hashtable;
+import java.util.Date;
 
 class ConsoleUI {
 
@@ -47,15 +55,21 @@ class ConsoleUI {
     }
 
     private void login(AccessLevel accessLevel) throws IOException {
-        String login;
-
         if (accessLevel == AccessLevel.PATIENT) {
-            print("Please enter you medical number:");
-            loginID = in.readLine();
+            print("Are you an already registered patient? (y/n)");
+            String answer = in.readLine();
+            if (answer.equals("n")) {
+                registerNewPatient();
+            }
+            else {
+                print("Please enter you medical number:");
+                loginID = in.readLine();
 
-            // TODO db stuff, check if medical nbr exists
+                // TODO db stuff, check if medical nbr exists
+            }
 
             print("Logged in as a patient");
+            patientMenu();
         }
         else if (accessLevel == AccessLevel.DOCTOR) {
             print("Please enter you employee number:");
@@ -84,7 +98,7 @@ class ConsoleUI {
         print("\t5. Add doctor");
         print("\t6. Add specialization");
         print("\t7. Delete doctor");
-        print("\t8. Logout");
+        print("\t8. Log out");
 
         answer = in.readLine();
         if (answer.equals("1")) {
@@ -204,7 +218,7 @@ class ConsoleUI {
         print("\t4. View medical records");
         print("\t5. Add medical record");
         print("\t6. Register unavailability");
-        print("\t7. Logout");
+        print("\t7. Log out");
 
         answer = in.readLine();
         if (answer.equals("1")) {
@@ -296,6 +310,217 @@ class ConsoleUI {
 
     private void registerUnavailability() {
         // TODO no idea how to approach this right now
+    }
+
+    private void patientMenu() throws IOException {
+        print("--- PATIENT MENU ---");
+        print("\t1. Book appointment");
+        print("\t2. View personal details");
+        print("\t3. Edit personal details");
+        print("\t4. Log out");
+
+        String answer = in.readLine();
+        if (answer.equals("1")) {
+            chooseDoctorToBook();
+            patientMenu();
+        }
+        else if (answer.equals("2")) {
+            viewPatientDetails(loginID);
+            patientMenu();
+        }
+        else if (answer.equals("3")) {
+            editPatientDetails(loginID);
+            patientMenu();
+        }
+        else if (answer.equals("4")) {
+            loginMenu();
+        }
+        else if (!answer.equalsIgnoreCase("exit")) {
+            print("Invalid input.");
+            patientMenu();
+        }
+    }
+
+    private void registerNewPatient() throws IOException {
+        String medicalNbr;
+        String fName;
+        String lName;
+        String gender;
+        String address;
+        String phoneNbr;
+        String birthYear;
+        String birthMonth;
+        String birthDay;
+
+        print("--- PATIENT REGISTRATION ---");
+        print("Please enter your medical number:");
+        medicalNbr = in.readLine();
+
+        print("Please enter your first name:");
+        fName = in.readLine();
+
+        print("Please enter your last name:");
+        lName = in.readLine();
+
+        print("Please enter your gender: (M/F)");
+        gender = in.readLine();
+
+        print("Please enter your address:");
+        address = in.readLine();
+
+        print("Please enter your phone number:");
+        phoneNbr = in.readLine();
+
+        // Use the following to build a date. I'm not sure how
+        // it should be formatted to be used in the DBMS.
+        print("Please enter the year you were born:");
+        birthYear = in.readLine();
+
+        print("Please enter the month you were born:");
+        birthMonth = in.readLine();
+
+        print("Please enter the day you were born:");
+        birthDay = in.readLine();
+
+        // TODO add a new patient, also add current date as registration date
+
+        loginID = medicalNbr;
+    }
+
+    private void chooseDoctorToBook() throws IOException {
+        // TODO get all doctors and their spec and cost.
+        ResultSet resultDoctors = null;
+        Hashtable<Integer, String> doctorMap = new Hashtable<>();
+
+        Integer row = 1;
+        try {
+            while (resultDoctors.next()) {
+                doctorMap.put(row, resultDoctors.getString("employee_number"));
+                print("\t" + row + ". "+resultDoctors.getNString("first_name")+
+                        " " + resultDoctors.getNString("last_name") +
+                        ", " + resultDoctors.getNString("specialization") +
+                        ", " + resultDoctors.getDouble("cost"));
+                row++;
+            }
+
+            if (doctorMap.isEmpty()) {
+                print("No doctors were found.");
+            }
+            else {
+                String answer = in.readLine();
+
+                int answerRow;
+                try {
+                    answerRow = Integer.parseInt(answer);
+                } catch (NumberFormatException e) {
+                    answerRow = -1;
+                }
+
+                if (!answer.equals("exit") && answerRow > 0 && answerRow <= row) {
+                    chooseAppointment(doctorMap.get(answerRow));
+                }
+                else {
+                    print("Invalid input.");
+                }
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Database error");
+        }
+    }
+
+    private void chooseAppointment(String employee_nbr) throws IOException {
+        // TODO get available times for next week, using employee_nbr
+        ResultSet resultTimes = null;
+        Hashtable<Integer, LocalDateTime> timesMap = new Hashtable<>();
+
+        Integer row = 1;
+        LocalDateTime dateTime;
+        try {
+            while (resultTimes.next()) {
+                dateTime = resultTimes.getTimestamp("datetime").toLocalDateTime();
+                timesMap.put(row, dateTime);
+
+                print("\t" + row + ". " + dateTime.getDayOfWeek().name() +
+                        " " + dateTime.getDayOfMonth() +
+                        "/ " + dateTime.getMonthValue() +
+                        ", " + dateTime.getHour() +
+                        ":" + dateTime.getMinute());
+                row++;
+            }
+
+            if (timesMap.isEmpty()) {
+                print("No available times were found.");
+            }
+            else {
+                String answer = in.readLine();
+
+                int answerRow;
+                try {
+                    answerRow = Integer.parseInt(answer);
+                } catch (NumberFormatException e) {
+                    answerRow = -1;
+                }
+
+                if (!answer.equals("exit") && answerRow > 0 && answerRow <= row) {
+                    bookAppointment(timesMap.get(answerRow), employee_nbr);
+                }
+                else {
+                    print("Invalid input.");
+                }
+            }
+        }
+        catch (SQLException e) {
+            System.err.println("Database error.");
+        }
+    }
+
+    private void bookAppointment(LocalDateTime dateTime, String employee_nbr) {
+
+        // TODO book appointment with dateTime, lognID and employee_nbr
+
+    }
+
+    private void viewPatientDetails(String medial_nbr) {
+
+        // TODO get patient information
+
+    }
+
+    private void editPatientDetails(String medial_nbr) throws IOException {
+        String Fname;
+        String Lname;
+        String gender;
+        String phone_nbr;
+        String birthYear;
+        String birthMonth;
+        String birthDay;
+
+        print("Press 'enter' if you wish to keep the current data.");
+
+        print("Please enter your first name:");
+        Fname = in.readLine();
+
+        print("Please enter your last name:");
+        Lname = in.readLine();
+
+        print("Please enter your gender:");
+        gender = in.readLine();
+
+        print("Please enter your phone number:");
+        phone_nbr = in.readLine();
+
+        print("Please enter the year you were born:");
+        birthYear = in.readLine();
+
+        print("Please enter the month you were born:");
+        birthMonth = in.readLine();
+
+        print("Please enter the day you born:");
+        birthDay = in.readLine();
+
+        // TODO update patient only with fields that aren't blank
+
     }
 
     void start() {
